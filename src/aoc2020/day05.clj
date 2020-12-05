e1b80ec4d40d42139d63483a686ac9a72323091a
Lars Nilsson <lars@quantumchamaeleon.com>
2020-12-05 11:56:24 -0500

    Improved day 6 seat calculation

--- src/aoc2020/day05.clj
+++ src/aoc2020/day05.clj
@@ -1,18 +1,15 @@
 (ns aoc2020.day05
   (:require [clojure.java.io :refer [reader resource]])
+  (:require [clojure.string :as str])
   (:gen-class))
-
-(def conversion {\F 0, \B 1, \L 0, \R 1})
 
 (defn load-data [filename]
   (line-seq (reader (resource filename))))
 
 (defn calculate-seat [seat]
-  (let [front-back (apply str (map conversion (filter #{\F \B} seat)))
-        left-right (apply str (map conversion (filter #{\L \R} seat)))
-        row (Integer/parseInt front-back 2)
-        col (Integer/parseInt left-right 2)]
-    (+ (* row 8) col)))
+  (let [seat (str/replace seat #"[FL]" "0")
+        seat (str/replace seat #"[BR]" "1")]
+    (Integer/parseInt seat 2)))
 
 (defn part1 []
   (let [data (load-data "day05.txt")]
