61949317e879f82acd1db5f0fe921e2acf32bee2
Lars Nilsson <lars@quantumchamaeleon.com>
2020-12-08 09:53:34 -0500

    Day 8 solutions

---
+++ src/aoc2020/day08.clj
@@ -0,0 +1,58 @@
+(ns aoc2020.day08
+  (:require [clojure.java.io :refer [reader resource]])
+  (:require [clojure.edn :as edn])
+  (:gen-class))
+
+(def instructions {"nop" :nop, "acc" :acc, "jmp" :jmp})
+
+(defn parse-instruction [instruction]
+  (let [[_ instruction data] (re-find #"^(\w+) (.+)" instruction)]
+    [(get instructions instruction)
+     (edn/read-string data)]))
+
+(defn load-data [filename]
+  (let [data (line-seq (reader (resource filename)))]
+    (mapv parse-instruction data)))
+
+(defmulti execute (fn [instruction _ _] (first instruction)))
+
+(defmethod execute :nop [[_ _] offset accum]
+  [(inc offset) accum])
+
+(defmethod execute :acc [[_ arg] offset accum]
+  [(inc offset) (+ accum arg)])
+
+(defmethod execute :jmp [[_ arg] offset accum]
+  [(+ offset arg) accum])
+
+(defmethod execute :default [[_ _] offset]
+  (throw (format "Unimplemented instruction at offset %d!" offset)))
+
+(defn run-until-loop [visited instructions offset accum]
+  (if (or (>= offset (count instructions))
+          (contains? visited offset))
+    [accum offset]
+    (let [instruction (get instructions offset)
+          [next-offset accum] (execute instruction offset accum)]
+      (recur (conj visited offset) instructions next-offset accum))))
+
+(defn part1 []
+  (let [instructions (load-data "day08.txt")]
+    (first (run-until-loop #{} instructions 0 0))))
+
+(def fix {:nop :jmp, :jmp :nop})
+
+(defn fix-instruction [[instruction arg]]
+  [(get fix instruction instruction) arg])
+
+(defn instruction-fix-seq [instructions]
+  (for [offset (range (count instructions))]
+    (assoc instructions offset (fix-instruction (get instructions offset)))))
+
+(defn part2 []
+  (let [instructions (load-data "day08.txt")
+        candidates (instruction-fix-seq instructions)
+        results (map #(run-until-loop #{} % 0 0) candidates)]
+    (->> (filter (fn [[_ offset]] (>= offset (count instructions))) results)
+         (first)
+         (first))))
